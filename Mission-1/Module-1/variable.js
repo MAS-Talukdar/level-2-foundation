14a3f59521e696520532e5287e44bddf792f78d3
MAS-Talukdar <mdabusufiantalukdar8@gmail.com>
2026-08-25 13:28:30 +0600

    first

---
+++ Mission-1/Module-1/variable.js
@@ -0,0 +1,27 @@
+// console.log("Variable Run Hoyeche");
+
+// ------------------------
+
+const name = 'Rahim';
+// name= 'Karim';
+console.log(name);
+
+// ------------------------
+
+let age = 21;
+age= 23;
+console.log(age);
+
+// -------------
+
+const isAdult = true;
+
+// --------------------
+console.log(typeof(name), typeof(age), typeof(isAdult));
+
+/*
+* Rahim replace by Karim
+* "let" is a updatable(reassign) variable, but "const" is a constant variable.
+* A name of thing is not change in the real life(in generally), therefore it's in const. And age of a thing is change in every year, therefore it's in let.
+! Memorization = total data types in JS shortcut "ssbbnnou= string, symbol, bigin, boolean, number, null, undefined, object"
+*/
