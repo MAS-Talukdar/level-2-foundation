90d6a5df8139e1b740c34122f7f823744fe8803a
MAS-Talukdar <mdabusufiantalukdar8@gmail.com>
2026-08-28 00:05:01 +0600

    Solve

--- Mission-1/Module-1/practices.js
+++ Mission-1/Module-1/practices.js
@@ -1,22 +1,23 @@
-function calcBmi(){
-    const weight = Number(document.getElementById("w").value)
-    const height = Number(document.getElementById("h").value)
-    const bmi = weight/ (height*height)
+const a = 10;
+const b = 3;
+const sum = a+b;
+console.log(sum);
 
-    if (bmi < 18.5){
-        document.getElementById("txt").innerText =`Your BMI is = ${bmi}`;
-        document.getElementById("com").innerText ="You are : Under Weight";
-    }
-    else if (bmi < 24.9){
-        document.getElementById("txt").innerText =`Your BMI is = ${bmi}`;
-        document.getElementById("com").innerText ="You are : Healthy Weight";
-    }
-    else if (bmi < 29.9){
-        document.getElementById("txt").innerText =`Your BMI is = ${bmi}`;
-        document.getElementById("com").innerText ="You are : Over Weight";
-    }
-    else{
-        document.getElementById("txt").innerText =`Your BMI is = ${bmi}`
-        document.getElementById("com").innerText ="You are : Obese";
-    }
-}
+const divide = a/b;
+console.log(divide);
+console.log(divide.toFixed(2));
+
+const rmndr = a%b;
+console.log(rmndr);
+
+// a + b - (c+f) * d / e
+
+sum = sum + 10; sum += 10;
+
+
+const c = a<b;
+console.log(c);
+
+
+// const c = !(a<b);
+// console.log(c);
